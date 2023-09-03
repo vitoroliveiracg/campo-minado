1b773e5838dfb7c2a9d2d067b4cbe4f8620f9d3a
Vitor Oliveira <118785739+vitoroliveiracg@users.noreply.github.com>
2023-09-03 20:41:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let firstsWhiteSpaces = 10
 
 let amountLines = 10
 let amountColumns = 25
+let blockSize = 35
+let playZoneSizeHeight = 350
+let playZoneSizeWidth = 875
+let root = document.querySelector(':root')
 
 let map = []
 let playZone = document.getElementById('playingzone')
@@ -12,35 +16,65 @@ let firstClick = true
 let losed = false
 let winned = false
 
+let intervalTimer
+let counter = 0
+
 reloadGame()
 function loadGame(){
   let dificult =  document.querySelector('#dificult')
 
   switch (dificult.selectedIndex) {
     case 0: //easy
-      qntBombs = 35
-      bombsCloseLimit = 3
+      qntBombs = 30
+      bombsCloseLimit = 2
       firstsWhiteSpaces = 20
+
+      amountLines = 10
+      amountColumns = 25
+      blockSize = 35
+      playZoneSizeHeight = 350
+      playZoneSizeWidth = 875
       break
     case 1: //normal
-      qntBombs = 45
+      qntBombs = 75
       bombsCloseLimit = 4
       firstsWhiteSpaces = 10
+
+      amountLines = 20
+      amountColumns = 35
+      blockSize = 25
+      playZoneSizeHeight = 500
+      playZoneSizeWidth = 875
       break
     case 2: //hard
-      qntBombs = 55
+      qntBombs = 190
       bombsCloseLimit = 5
       firstsWhiteSpaces = 7
+
+      amountLines = 35
+      amountColumns = 35
+      blockSize = 25
+      playZoneSizeHeight = 875
+      playZoneSizeWidth = 875
       break
   }
+
 }
 
 document.querySelector('#reload').addEventListener('click',reloadGame)
 
 function reloadGame(){
   loadGame()
-  console.log(qntBombs)
 
+  root.style.setProperty('--block-height',`${blockSize}px`)
+  root.style.setProperty('--block-width',`${blockSize}px`)
+
+  root.style.setProperty('--playing-zone-height',`${playZoneSizeHeight}px`)
+  root.style.setProperty('--playing-zone-width',`${playZoneSizeWidth}px`)
+
+  stopTimer(intervalTimer)
+  resetTimer()
+  counter = 0
   playZone.innerHTML = ''
   map = []
   firstClick = true
@@ -52,9 +86,9 @@ function reloadGame(){
 
 
 function fillMap(){
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < amountLines; i++) {
     map[i] = []
-    for (let j = 0; j < 25; j++) {
+    for (let j = 0; j < amountColumns; j++) {
       let block = document.createElement('div')
       block.id = `${i},${j}`
       block.className = 'block'
@@ -76,6 +110,7 @@ function loadBlocks(){
           openFirstSpace(elemLine, elemColumn)
           plantbombs()
           calculateNumbers()
+          startTimer()
         }
         openSpace(elemLine, elemColumn)
         winGame()
@@ -105,27 +140,49 @@ function loadBlocks(){
 }
 
 
+function startTimer(){
+  let timer = document.querySelector('#timer')
+
+
+  intervalTimer = setInterval(() => {
+      if(counter <= 999)
+        counter++
+      timer.innerText = String(counter).padStart(3,0)
+  }, 1000);
+
+}
+
+function resetTimer(){
+  let timer = document.querySelector('#timer')
+  timer.innerText = '000'
+}
+
+function stopTimer(){
+  clearInterval(intervalTimer)
+}
+
 function winGame(){
   if(losed !== true){
     let count = 0
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 25; j++) {
+    for (let i = 0; i < amountLines; i++) {
+      for (let j = 0; j < amountColumns; j++) {
         let elem = document.getElementById(`${i},${j}`)
         if(elem.classList.contains('white') || map[i][j] === 'bomb'){
           count++
         }
       }
     }
-    if(count == (10 * 25)){
+    if(count == (amountLines * amountColumns)){
       winned = true
+      stopTimer()
       alert('YOU WIN!')
     }
   }
 }
 
 function calculateNumbers(){
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 25; j++) {
+  for (let i = 0; i < amountLines; i++) {
+    for (let j = 0; j < amountColumns; j++) {
       if(map[i][j] === null){
         let element = document.getElementById(`${i},${j}`)
         let bombsNearby = calculateBombsNearby(i,j, 'bomb')
@@ -146,17 +203,19 @@ function calculateNumbers(){
 
 function calculateBombsNearby(line, column, whatToSearch){  
   let bombsClose = 0
+  let limitLine = (amountLines - 1)
+  let limitColumn = (amountColumns - 1)
 
   bombsClose += (column != 0)?((line != 0)?((map[line - 1][column - 1] === whatToSearch)? 1: 0): 0): 0 //top left
   bombsClose += (column != 0)?((map[line][column - 1] === whatToSearch)? 1: 0): 0 //top middle
-  bombsClose += (column != 0)?((line != 9)?((map[line + 1][column - 1] === whatToSearch)? 1: 0): 0): 0 //top right
+  bombsClose += (column != 0)?((line != limitLine)?((map[line + 1][column - 1] === whatToSearch)? 1: 0): 0): 0 //top right
 
   bombsClose += (line != 0)?((map[line - 1][column] === whatToSearch)? 1: 0): 0 // middle left
-  bombsClose += (line != 9)?((map[line + 1][column] === whatToSearch)? 1: 0): 0 //middle right
+  bombsClose += (line != limitLine)?((map[line + 1][column] === whatToSearch)? 1: 0): 0 //middle right
 
-  bombsClose += (column != 24)?((line != 0)?((map[line - 1][column + 1] === whatToSearch)? 1: 0): 0): 0 //bottom left
-  bombsClose += (column!= 24)?((map[line][column + 1] === whatToSearch)? 1: 0): 0 //bottom middle
-  bombsClose += (column != 24)?((line != 9)?((map[line + 1][column + 1] === whatToSearch)? 1: 0): 0): 0 //bottom right
+  bombsClose += (column != limitColumn)?((line != 0)?((map[line - 1][column + 1] === whatToSearch)? 1: 0): 0): 0 //bottom left
+  bombsClose += (column!= limitColumn)?((map[line][column + 1] === whatToSearch)? 1: 0): 0 //bottom middle
+  bombsClose += (column != limitColumn)?((line != limitLine)?((map[line + 1][column + 1] === whatToSearch)? 1: 0): 0): 0 //bottom right
 
   return bombsClose
 }
@@ -175,8 +234,8 @@ function openSpace(line, column){
 
   do {
     whitesNearby = 0
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 25; j++) {
+    for (let i = 0; i < amountLines; i++) {
+      for (let j = 0; j < amountColumns; j++) {
         if(calculateBombsNearby(i,j,'white') > 0){
           if(map[i][j] === 0){
             whitesNearby = 1
@@ -193,8 +252,8 @@ function openSpace(line, column){
 
 function loseGame(){
   if(winned !== true){
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 25; j++) {
+    for (let i = 0; i < amountLines; i++) {
+      for (let j = 0; j < amountColumns; j++) {
         let elem = document.getElementById(`${i},${j}`)
         if(map[i][j] !== 'bomb'){
           elem.classList.add('white')
@@ -204,6 +263,7 @@ function loseGame(){
       }    
     }
   
+    stopTimer()
     losed = true
     alert('you lose!')
   }
@@ -213,8 +273,8 @@ function openFirstSpace(line, column){
   let whiteSpaces = firstsWhiteSpaces
   map[line][column] = 'white'
   do {
-    let randomLine = getRandomInt(0,10)
-    let randomColumn = getRandomInt(0,25)
+    let randomLine = getRandomInt(0,amountLines)
+    let randomColumn = getRandomInt(0,amountColumns)
     if(checkSurroundings(randomLine,randomColumn,'white')){
       map[randomLine][randomColumn] = 'white'
       whiteSpaces--
@@ -227,13 +287,11 @@ function openFirstSpace(line, column){
 function plantbombs(){
   bombs = qntBombs
   do {
-    let randomLine = getRandomInt(0,10)
-    let randomColumn = getRandomInt(0,25)
+    let randomLine = getRandomInt(0,amountLines)
+    let randomColumn = getRandomInt(0,amountColumns)
     if(checkSurroundingsBombs(randomLine, randomColumn, 'bomb')){
       map[randomLine][randomColumn] = 'bomb'
       bombs--
-      // let block = document.getElementById(`${randomLine},${randomColumn}`)
-      // block.classList.add('bomb')
     }
     
   } while (bombs != 0);
@@ -242,8 +300,8 @@ function plantbombs(){
 function checkSurroundings(line, column, whatToSearch){
   let toReduceLine = (line == 0)? 0: 1
   let toReduceColumn = (column == 0)? 0: 1
-  let toIncreaseLine = (line == 9)? 0: 1
-  let toIncreaseColumn = (column == 24)? 0: 1
+  let toIncreaseLine = (line == (amountLines - 1))? 0: 1
+  let toIncreaseColumn = (column == (amountColumns - 1))? 0: 1
   return (
     map[line][column] !== whatToSearch && (
     map[line][column - toReduceColumn] === whatToSearch ||
@@ -260,8 +318,6 @@ function checkSurroundingsBombs(line, column, whatToSearch){
     (calculateBombsNearby(line,column,'white') === 0)
   )
 }
-
-console.log(map)
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
